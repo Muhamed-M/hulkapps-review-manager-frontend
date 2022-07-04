02815b32e8009f0f42903b85ba4685c4bc99c8fc
Muhamed-M <muhamedmuhamedagic03@gmail.com>
2022-07-04 07:58:48 +0200

    heroku

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: ['vuetify'],
     devServer: {
-        proxy: 'http://localhost:5000',
+        // proxy: 'http://localhost:5000',
+        allowedHosts: 'all',
     },
 })
